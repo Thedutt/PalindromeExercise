330f038fff52a45740fc69fc4bf98d79af21ee42
John Dutton <johnmdutton1@gmail.com>
2020-09-14 14:43:15 -0500

    first version of the palindrome

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    { 
+        public bool IsAPalindrome(string word)
+        {
+            var reversed = "";
+
+            for(int i=word.Length-1; i>=0 ;i--)
+            {
+                reversed += word[i];
+            }
+
+            if(reversed.ToLower() == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+           
+        }
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,21 @@
+using PalindromeExercise;
 using System;
 using Xunit;
+
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar",true)]
+        [InlineData("hello",false)]
+        [InlineData("Racecar",true)]
+        public void Test1(string word, bool expected)
         {
+            var test = new WordSmith();
+            bool actual = test.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
 
         }
     }
